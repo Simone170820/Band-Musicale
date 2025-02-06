951fa32da340b01798f4f83b1cf45f2f1e4f5c41
Simone170820 <simonepontillo17@icloud.com>
2025-02-06 09:11:43 +0100

    Delete backend directory

--- backend/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Distrugge la sessione
-header("Location: login.php"); // Torna alla pagina di login
-exit();
-?>
--- backend/test_connessione.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'config.php';
-echo "Connessione riuscita!";
-?>
